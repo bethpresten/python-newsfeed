05b9378188e547c23ce78c0b0afea41e1447480c
Beth Presten <bethpresten@gmail.com>
2021-02-27 19:18:23 -0500

    added filter function to edit-post, comments, post-info, and added it into the utils folder

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -10,3 +11,19 @@ load_dotenv()
 engine = create_engine("mysql+pymysql://root:PASSWORD@localhost/python_news_db")
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+  Base.metadata.create_all(engine)
+
+  app.teardown_appcontext(close_db)
+def get_db():
+  if 'db' not in g:
+    # store db connection in app context
+    g.db = Session()
+
+  return g.db
+def close_db(e=None):
+  db = g.pop('db', None)
+
+  if db is not None:
+    db.close()
