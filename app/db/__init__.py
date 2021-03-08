25db82a15ee294f24db37fe91246a12e825acd49
Beth Presten <bethpresten@gmail.com>
2021-03-08 18:33:26 -0500

    changed two files for database purposes

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -8,7 +8,7 @@ from flask import g
 load_dotenv()
 
 # connect to database using env variable
-engine = create_engine("mysql+pymysql://root:PASSWORD@localhost/python_news_db")
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
